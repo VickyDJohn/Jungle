e353a3820eb13974f9b1751dd265a075cc104959
Vicky John <vicky.d.john@email.com>
2023-09-27 16:03:58 -0700

    add admin auth using HTTP basic

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module New
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    Dotenv::Railtie.load
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
