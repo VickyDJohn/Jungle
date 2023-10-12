4fbd34a9689b38053d226b620c1af8c01fc8f308
Vicky John <vicky.d.john@email.com>
2023-10-11 20:22:54 -0700

    add validations for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:error] = @user.errors.full_messages
       redirect_to '/signup'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true
+  validates_confirmation_of :password
 end
