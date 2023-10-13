123fff429a5d25b994711048815692b975980aea
Vicky John <vicky.d.john@email.com>
2023-10-13 12:31:05 -0700

    add test to add to cart and update cart by 1

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,13 +1,13 @@
 describe('home page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
-  })
+    cy.visit('http://localhost:3000/');
+  });
 
-  it('displays all products on the page', ()=> {
-    cy.get(".products article").should("be.visible")
-  })
+  it('displays all products on the page', () => {
+    cy.get(".products article").should("be.visible");
+  });
 
-  it('displays 12 products', ()=> {
+  it('displays 12 products', () => {
     cy.get(".products article").should("have.length", 12);
-  })
-})
+  });
+});
