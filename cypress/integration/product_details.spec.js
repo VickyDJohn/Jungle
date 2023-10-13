61aa0eed4270a0243583e403ad0154e48c3eb143
Vicky John <vicky.d.john@email.com>
2023-10-13 12:08:56 -0700

    add tests to click on second and last product

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -3,7 +3,15 @@ describe('product details', () => {
     cy.visit('http://localhost:3000/');
   });
 
-  it("displays product details when clicked", () => {
+  it("displays the first product's details when clicked", () => {
     cy.get(".products article").first().click()
+  })
+
+  it("displays the second product's details when clicked", () => {
+    cy.get(".products article").eq(1).click()
+  })
+
+  it("displays the last product's details when clicked", () => {
+    cy.get(".products article").last().click()
   })
 });
