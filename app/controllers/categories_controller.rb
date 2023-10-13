3c245a79761b7408e8aa88b495afba7ed904cbdb
Vicky John <vicky.d.john@email.com>
2023-10-13 11:23:25 -0700

    add admin auth for admin pages only

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,5 @@
 class CategoriesController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
-
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
